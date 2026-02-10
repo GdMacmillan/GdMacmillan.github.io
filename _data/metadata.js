456ff87732d2588617c3d33af9c05fe17931db89
Gordon MacMillan <gmacilla@ymail.com>
2026-02-09 19:38:46 -0700

    Initialize Eleventy project structure

---
+++ _data/metadata.js
@@ -0,0 +1,6 @@
+export default {
+  title: "Gordon MacMillan",
+  description: "Data Science Professional",
+  url: "https://gdmacmillan.github.io",
+  author: { name: "Gordon MacMillan", email: "gmacilla@ymail.com" }
+};
